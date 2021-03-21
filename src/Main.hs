59267e6e6a5e21c457e2b41a9e694c7f351d7546
Carlos Gómez <carliros.g@gmail.com>
2021-03-20 21:11:08 -0400

    show basic gui with gtk. Register its dependencies.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,12 @@
 module Main where
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.IORef
+import Graphics.UI.Gtk hiding (Action, backspace)
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  void initGUI
+  window <- windowNew
+  widgetShowAll window
+  mainGUI
